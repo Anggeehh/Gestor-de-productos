051596f7049954c4b5a03efae1d25f1a73702522
Angel Piñero Orellana <apinore0703@g.educaand.es>
2025-10-21 09:41:32 +0200

    nuevos botones

--- Ventana.py
+++ Ventana.py
@@ -28,14 +28,15 @@ campo_stock = crear_label("Stock:", 5)
 frame_botones = tk.Frame(ventana, bg="#e6f2e6")
 frame_botones.pack(pady=10)
 
-def crear_boton(texto, columna):
-    boton = tk.Button(frame_botones, text=texto, bg="#b2d8b2", fg="#004d00", activebackground="#a3cfa3")
-    boton.grid(row=0, column=columna, padx=5)
-    return boton
+#---botones---
+boton_add = tk.Button(frame_botones, text="Añadir Tarea" , bg="#b2d8b2", fg="#004d00", activebackground="#a3cfa3")
+boton_update = tk.Button(frame_botones, text="Modificar Tarea" , bg="#b2d8b2", fg="#004d00", activebackground="#a3cfa3")
+boton_delete = tk.Button(frame_botones, text="Eliminar Tarea" , bg="#b2d8b2", fg="#004d00", activebackground="#a3cfa3")
 
-boton_add = crear_boton("Añadir Producto", 0)
-boton_update = crear_boton("Modificar Producto", 1)
-boton_delete = crear_boton("Eliminar Producto", 2)
+boton_add.grid(row=6, column=1, padx=10, pady=10)
+boton_update.grid(row=6, column=2, padx=10, pady=10)
+boton_delete.grid(row=6, column=3, padx=10, pady=10)
+
 
 # --- Lista de productos ---
 etiqueta_lista = tk.Label(ventana, text="Lista de productos:", bg="#e6f2e6", fg="#2e8b57")
@@ -43,5 +44,13 @@ etiqueta_lista.pack()
 lista_tareas = tk.Listbox(ventana, width=60, height=10, bg="#f0fff0", fg="#006400")
 lista_tareas.pack(padx=10, pady=5)
 
+
 ventana.mainloop()
+
+
+
+
+
+
+
 
