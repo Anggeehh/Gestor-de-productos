61fe2c489c1a9e179078398b7ea84e596c2decf4
Angel Piñero Orellana <apinore0703@g.educaand.es>
2025-11-04 08:41:10 +0100

    Base de datos en el código principal

--- Ventana.py
+++ Ventana.py
@@ -63,6 +63,36 @@ class App:
     def eliminado(self):
         print("Has eliminado un producto")
 
+    # --- PASO 1: Conectar a la base de datos ---
+# Se crea el archivo 'tareas.db' si no existe
+conexion = sqlite3.connect('productos.db')
+
+# Para poder enviar comandos, necesitamos un "cursor"
+cursor = conexion.cursor()
+
+# --- PASO 2: Ejecutar un comando SQL ---
+# Usamos un string multilínea con triples comillas para que el SQL sea más legible
+comando_sql = """
+CREATE TABLE IF NOT EXISTS producto (
+    id INTEGER PRIMARY KEY,
+    nombre TEXT ,
+    fecha_caducidad TEXT,
+    precio TEXT,
+    categoria TEXT,
+    marca TEXT, 
+    stock INTEGER
+)
+"""
+# 'IF NOT EXISTS' evita que nos dé un error si la tabla ya ha sido creada
+cursor.execute(comando_sql)
+
+# Para que los cambios se guarden de forma permanente, hacemos un "commit"
+conexion.commit()
+
+# --- PASO 3: Cerrar la conexión ---
+conexion.close()
+
+print("Tabla 'Producto' creada con éxito (si no existía ya).")
 
 # --- Lanzar la aplicación ---
 if __name__ == "__main__":
